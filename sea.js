7c4369bf7f945aa86bc7c1953df9820c8cb12b6d
bencelder <ben@yukawa.local>
2015-10-02 23:11:57 -0400

    cursor and bullets

--- sea.js
+++ sea.js
@@ -1,44 +1,160 @@
 function initialize(){
     c = document.getElementById("myCanvas");
     ctx = c.getContext("2d");
+    c.style.cursor = "none";
 
+    cursor = new Cursor(0, 0);
+
+    paused = -1; // -1 = not paused, 1 = paused
     keys = []
     player = new Player( c.width / 2., c.height/2. );
 
+    bullets = [];
+
+    var music = new Audio("tangerine_dreams.mp3");
+    //music.play();
+
+    lastframe = Date.now();
     game_loop = setInterval( function(){loop()}, 1);
 }
 
+function sign(x){
+    if (x > 0)
+        return 1;
+    return -1;
+}
+
 function loop(){
-    ctx.fillStyle = "#263238";
+    now = Date.now();
+    dt = (now - lastframe)/1000; // ms
+    lastframe = now;
+
+    if (paused == 1)
+        return;
+
+    player.heading = [0, 0];
+
+    // input
+    if (keys[65])
+        player.heading[0] = -1;
+    if (keys[68])
+        player.heading[0] = 1;
+    if (keys[83])
+        player.heading[1] = 1;
+    if (keys[87])
+        player.heading[1] = -1;
+
+    // make a new bullet
+    if (now - player.last_bullet > player.max_time_since_bullet){
+        dx = cursor.x - player.x;
+        dy = cursor.y - player.y;
+        vel = norm_vec( [dx, dy] );
+        vel = mul_vec( 600, vel );
+        b = new Bullet( player.x, player.y, vel[0], vel[1] );
+        bullets.push(b);
+        player.last_bullet = now;
+    }
+
+    player.update(dt);
+
+    for (var i = 0; i < bullets.length; i++)
+        bullets[i].update(dt)
+
+
+    // draw
+    ctx.fillStyle = "#002447";
     ctx.fillRect(0, 0, c.width, c.height);
 
-    if (keys[65])
-        player.x -= 1;
-    if (keys[68])
-        player.x += 1;
-    if (keys[83])
-        player.y += 1;
-    if (keys[87])
-        player.y -= 1;
-
+    cursor.draw();
     player.draw();
+
+    for (var i = 0; i < bullets.length; i++)
+        bullets[i].draw();
+
+
+}
+
+function Cursor(x, y){
+    this.x = x;
+    this.y = y;
+
+    this.draw = function(){
+        ctx.strokeStyle = "#FFFFFF";
+        ctx.lineWidth = 3;
+
+        var pi = Math.PI;
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 5, pi/5 - pi/2, 4*pi/5 - pi/2);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 5, 6*pi/5 - pi/2, 9*pi/5 - pi/2);
+        ctx.stroke();
+    }
+}
+
+function Bullet(x, y, vx, vy){
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+
+    this.update = function(dt){
+        this.x += vx * dt;
+        this.y += vy * dt;
+    }
+
+    this.draw = function(){
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillRect( this.x - 5, this.y - 5, 10, 10 );
+    }
 }
 
 function Player(x, y){
     this.x = x;
     this.y = y;
 
+    this.heading = [0, 0];
+
+    this.vel = 150;
+
+    this.max_time_since_bullet = 500; // ms
+    this.last_bullet = 0;
+
     this.draw = function(){
         ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect( this.x, this.y, 10, 10 );
+        ctx.fillRect( this.x - 5, this.y - 5, 10, 10 );
+    }
+
+    this.update = function(dt){
+        //v = this.heading;
+        v = norm_vec( this.heading );
+        v = mul_vec( dt*this.vel, v );
+        this.x += v[0];
+        this.y += v[1];
+
+        this.x = Math.max(0, this.x);
+        this.x = Math.min(c.width, this.x);
+        this.y = Math.max(0, this.y);
+        this.y = Math.min(c.height, this.y);
     }
 }
 
 function keyDown(e){
     //alert(e.keyCode);
-    keys[e.keyCode] = true;
+    kc = e.keyCode;
+    keys[kc] = true;
+
+    if (kc == 80)
+        paused *= -1;
 }
 
 function keyUp(e){
     keys[e.keyCode] = false;
+}
+
+function mouseMove(e){
+    cursor.x = e.clientX - c.offsetLeft;
+    cursor.y = e.clientY - c.offsetTop;
 }
---
+++ vector.js
@@ -0,0 +1,31 @@
+function add_vec( v1, v2 ){
+    v = [];
+    for (var i = 0; i < v1.length; i++)
+        v.push( v1[i] + v2[i] );
+    return v;
+}
+
+function dot_vec( v1, v2 ){
+    sum = 0;
+    for (var i = 0; i < v1.length; i++)
+        sum += v1[i] * v2[i] ;
+    return sum;
+}
+
+function mul_vec( a, v1 ){
+    v = [];
+    for (var i = 0; i < v1.length; i++)
+        v.push( a * v1[i] );
+    return v;
+}
+
+function norm_vec( v1 ){
+    mag = dot_vec(v1, v1);
+    mag = Math.sqrt(mag);
+
+    if (mag == 0)
+        return [0, 0];
+
+    v =  mul_vec( 1./mag, v1 );
+    return v;
+}
